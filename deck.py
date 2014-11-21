57e4755a790341670408222e0197516d77e659d0
Adnan Duric <aduric@gmail.com>
2014-11-21 02:32:05 +0000

    Adds deck ctor

--- deck.py
+++ deck.py
@@ -1,4 +1,9 @@
+from card import Card, Rank, Suit
+
 class Deck(object):
+
+	def __init__(self):
+		self.cards = [(x,y) for x in Rank for y in Suit]
 
 	def shuffle(self):
 		pass
--- tests.py
+++ tests.py
@@ -4,10 +4,17 @@ from deck import Deck
 
 class HighCardTest(unittest.TestCase):
 
+
+	def test_deck_creation(self):
+		deck = Deck()
+		self.assertEquals((Rank.Two, Suit.Diamond), deck.cards[0])
+
+
 	def test_deck_shuffle_sufficient_randomization(self):
 		deck = Deck()
 		result = deck.shuffle()
 		self.assertEqual(4, result)
+
 
 	def test_card_creation(self):
 		card = Card(Rank.Ace, Suit.Spade)
